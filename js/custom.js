bbbfa5212fa55aab8d6ddb11d58aaca9120c7abc
syahrundalu <dalu.syahrun@gmail.com>
2023-02-08 22:57:50 +0700

    Miso Tessera Website

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height();
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
+  $(document).ready(function () {
+    //Owl
+
+    $('#projects-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        items: 2,
+        dots: true,
+        smartSpeed: 600,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2,
+                margin: 8,
+            }
+        }
+    })
+
+});
+
+
